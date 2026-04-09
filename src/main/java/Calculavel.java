5e8d103100ca0f17fc6ab3b705e18e55c439a725
LucasRaphaEgidio <lucasegidio1604@gmail.com>
2026-04-09 14:34:44 -0300

    Delete src/main/java directory

--- src/main/java/Calculavel.java
+++
@@ -1,5 +0,0 @@
-//Lucas Raphael, 2525518 - Mateus Rodrigues, 2503700 - Norberto Junior, 2209136
-
-public interface Calculavel {
-    public double calcular();
-}
--- src/main/java/PrecoInvalException.java
+++
@@ -1,7 +0,0 @@
-//Lucas Raphael, 2525518 - Mateus Rodrigues, 2503700 - Norberto Junior, 2209136
-
-public class PrecoInvalException extends Exception {
-    public PrecoInvalException(String mensagem) {
-        super(mensagem);
-    }
-}
